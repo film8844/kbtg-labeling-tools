cc08863ecb26c5dbaea24d07279b25dc954c3dab
Film_z <ffeem59@gmail.com>
2023-10-11 03:02:23 +0700

    ✨ edit box feature on Object detection

--- src/templates/example/drowinv2.js
+++ src/templates/example/drowinv2.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const zoomLevelSpan = document.getElementById('zoom-level');
     const deleteButton = document.getElementById('delete-button');
     const colorPicker = document.getElementById('color-picker');
+    let resizing = false;
+    let currentHandle = null;
 
     let zoomLevel = 100;
     let drawing = false;
@@ -59,6 +61,26 @@ document.addEventListener('DOMContentLoaded', function () {
         annotation.style.top = `${box.y * zoomLevel / 100}px`;
         annotation.style.width = `${box.width * zoomLevel / 100}px`;
         annotation.style.height = `${box.height * zoomLevel / 100}px`;
+
+        ['nw', 'ne', 'sw', 'se','m'].forEach(pos => {
+            const handle = document.createElement('div');
+            handle.className = `resize-handle ${pos}`;
+            handle.style.position = 'absolute';
+            if (pos.includes('n')) handle.style.top = '-4px';
+            if (pos.includes('s')) handle.style.bottom = '-4px';
+            if (pos.includes('w')) handle.style.left = '-4px';
+            if (pos.includes('e')) handle.style.right = '-4px';
+            if (pos === 'm') {
+                handle.style.top = '50%';
+                handle.style.left = '50%';
+                handle.style.transform = 'translate(-50%, -50%)';
+                handle.style.borderRadius = '50%';
+            }
+            handle.style.width = '8px';
+            handle.style.height = '8px';
+            handle.style.backgroundColor = box.borderColor;
+            annotation.appendChild(handle);
+        });
 
         container.appendChild(annotation);
     }
@@ -181,5 +203,59 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+    container.addEventListener('mousedown', function (event) {
+        const handle = event.target.closest('.resize-handle');
+        if (handle) {
+            resizing = true;
+            currentHandle = handle;
+            console.log(currentHandle);
+        }
+    });
+    
+    document.addEventListener('mouseup', function () {
+        resizing = false;
+        currentHandle = null;
+    });
+
+    container.addEventListener('mousemove', function (event) {
+        if (!resizing || !currentHandle) return;
+        const rect = image.getBoundingClientRect();
+        const x = (event.clientX - rect.left) * 100 / zoomLevel;
+        const y = (event.clientY - rect.top) * 100 / zoomLevel;
+        const box = boxes[parseInt(currentHandle.parentElement.dataset.index)];
+        const canses = currentHandle.className.split(' ')[1]
+    
+        switch (canses) {
+            case 'nw':
+                box.width += box.x - x;
+                box.height += box.y - y;
+                box.x = x;
+                box.y = y;
+                break;
+            case 'ne':
+                box.width = x - box.x;
+                box.height += box.y - y;
+                box.y = y;
+                break;
+            case 'sw':
+                box.width += box.x - x;
+                box.height = y - box.y;
+                box.x = x;
+                break;
+            case 'se':
+                box.width = x - box.x;
+                box.height = y - box.y;
+                break;
+            case 'm':
+                const deltaX = x - (box.x + box.width/2);
+                const deltaY = y - (box.y + box.height/2);
+                box.x += deltaX;
+                box.y += deltaY;
+        }
+    
+        updateAnnotations();
+        updateList();
+    });
+    
 
 });
